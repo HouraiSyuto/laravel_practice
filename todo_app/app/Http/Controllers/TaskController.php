b45bda84df347ad93a71e6309ed7be429ae90f7d
hourai <h.abcgn.s@gmail.com>
2019-10-08 03:07:47 +0900

    showEditFormメソッドの追加

--- todo_app/app/Http/Controllers/TaskController.php
+++ todo_app/app/Http/Controllers/TaskController.php
@@ -35,17 +35,29 @@ class TaskController extends Controller
     }
 
     public function create(int $id, CreateTask $request)
-{
-    $current_folder = Folder::find($id);
+    {
+        $current_folder = Folder::find($id);
 
-    $task = new Task();
-    $task->title = $request->title;
-    $task->due_date = $request->due_date;
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
 
-    $current_folder->tasks()->save($task);
+        $current_folder->tasks()->save($task);
 
-    return redirect()->route('tasks.index', [
-        'id' => $current_folder->id,
-    ]);
-}
+        return redirect()->route('tasks.index', [
+            'id' => $current_folder->id,
+        ]);
+    }
+
+    /**
+     * GET /folders/{id}/tasks/{task_id}/edit
+     */
+    public function showEditForm(int $id, int $task_id)
+    {
+        $task = Task::find($task_id);
+
+        return view('tasks/edit', [
+            'task' => $task,
+        ]);
+    }
 }
